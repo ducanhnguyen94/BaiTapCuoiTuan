7bf4446b0c88bcc0b4406d5674a785d443d54b8d
ducanhnguyen94 <ndanh4198@gmail.com>
2025-05-17 18:44:14 +0700

    BaiTapCuoiTuan

--- Bai7/script.js
+++ Bai7/script.js
@@ -26,55 +26,55 @@ let phepTinhThat = []
 one.addEventListener('click', () => {
     phepTinh.push(1);
     phepTinhThat.push(1);
-    result.innerHTML = phepTinh.join(' ')
+    result.innerHTML = phepTinh.join('')
 })
 
 two.addEventListener('click', () => {
     phepTinh.push(2);
     phepTinhThat.push(2);
-    result.innerHTML = phepTinh.join(' ')
+    result.innerHTML = phepTinh.join('')
 })
 
 three.addEventListener('click', () => {
     phepTinh.push(3);
     phepTinhThat.push(3);
-    result.innerHTML = phepTinh.join(' ')
+    result.innerHTML = phepTinh.join('')
 })
 
 four.addEventListener('click', () => {
     phepTinh.push(4);
     phepTinhThat.push(4);
-    result.innerHTML = phepTinh.join(' ')
+    result.innerHTML = phepTinh.join('')
 })
 
 five.addEventListener('click', () => {
     phepTinh.push(5);
     phepTinhThat.push(5);
-    result.innerHTML = phepTinh.join(' ')
+    result.innerHTML = phepTinh.join('')
 })
 
 six.addEventListener('click', () => {
     phepTinh.push(6);
     phepTinhThat.push(6);
-    result.innerHTML = phepTinh.join(' ')
+    result.innerHTML = phepTinh.join('')
 })
 
 seven.addEventListener('click', () => {
     phepTinh.push(7);
     phepTinhThat.push(7);
-    result.innerHTML = phepTinh.join(' ')
+    result.innerHTML = phepTinh.join('')
 })
 
 eight.addEventListener('click', () => {
     phepTinh.push(8);
     phepTinhThat.push(8);
-    result.innerHTML = phepTinh.join(' ')
+    result.innerHTML = phepTinh.join('')
 })
 
 nine.addEventListener('click', () => {
     phepTinh.push(9);
     phepTinhThat.push(9);
-    result.innerHTML = phepTinh.join(' ')
+    result.innerHTML = phepTinh.join('')
 })
 
 zero.addEventListener('click', () => {
@@ -83,50 +83,56 @@ zero.addEventListener('click', () => {
     } else {
         phepTinh.push(0);
         phepTinhThat.push(0);
-        result.innerHTML = phepTinh.join(' ')
+        result.innerHTML = phepTinh.join('')
     }   
 })
 
 moNgoac.addEventListener('click', () => {
     phepTinh.push('(');
     phepTinhThat.push('(');
-    result.innerHTML = phepTinh.join(' ')
+    result.innerHTML = phepTinh.join('')
 })
 
 dongNgoac.addEventListener('click', () => {
     phepTinh.push(')');
     phepTinhThat.push(')');
-    result.innerHTML = phepTinh.join(' ')
+    result.innerHTML = phepTinh.join('')
 })
 
 phepTru.addEventListener('click', () => {
     phepTinh.push('-');
     phepTinhThat.push('-');
-    result.innerHTML = phepTinh.join(' ')
+    result.innerHTML = phepTinh.join('')
 })
 
 phepCong.addEventListener('click', () => {
     phepTinh.push('+');
     phepTinhThat.push('+');
-    result.innerHTML = phepTinh.join(' ')
+    result.innerHTML = phepTinh.join('')
 })
 
 phepNhan.addEventListener('click', () => {
     phepTinh.push('x');
     phepTinhThat.push('*');
-    result.innerHTML = phepTinh.join(' ')
+    result.innerHTML = phepTinh.join('')
 })
 
 phepChia.addEventListener('click', () => {
     phepTinh.push('/');
     phepTinhThat.push('/');
-    result.innerHTML = phepTinh.join(' ')
+    result.innerHTML = phepTinh.join('')
 })
 
 phanTram.addEventListener('click', () => {
     phepTinh.push('%');
     phepTinhThat.push('/100');
-    result.innerHTML = phepTinh.join(' ')
+    result.innerHTML = phepTinh.join('')
+})
+
+dot.addEventListener('click', () => {
+    phepTinh.push('.');
+    phepTinhThat.push('.');
+    result.innerHTML = phepTinh.join('')
 })
 
 AC.addEventListener('click', () => {
